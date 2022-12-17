0760bddb08fd516c488a725df5a81920f7003ad9
andriy <Andruxan1989@gmail.com>
2022-12-17 21:42:34 +0200

    final edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, {useMemo, useState} from "react";
 import "./styles/App.css";
 import UserCard from "./components/UserCard/UserCard";
 import { UserDto } from "./api/contracts";
@@ -11,17 +11,12 @@ function App() {
       .then((data) => setUsers(data.results));
   };
 
-  const groupList = useMemo(
-    () =>
-      users.reduce((acc: Record<string, UserDto[]>, item: UserDto) => {
-        const date = new Date(item.registered.date).getFullYear();
-        if (!acc[date]) acc[date] = [item];
-        else acc[date].push(item);
-
-        return acc;
-      }, {}),
-    [users]
-  );
+  const groupList = useMemo( () => users.reduce((acc: Record<string, UserDto[]>, item: UserDto) => {
+      const date = new Date(item.registered.date).getFullYear()
+      if (!acc[date]) acc[date] = [item]
+      else acc[date].push(item)
+      return acc
+  }, {}), [users])
 
   return (
     <div className="App">
@@ -29,25 +24,18 @@ function App() {
         <h1>ARRAY METHOD REDUCE</h1>
         <h2>Group items by dates</h2>
         <button onClick={fetchData} className="button">
-          GET COMMENTS
+          GET USERS
         </button>
       </div>
       <div className="rightSection">
         {!users.length ? (
-          <h2>No comments yet</h2>
+          <h2>No users yet</h2>
         ) : (
           // users.map((user, i) => <UserCard key={i} user={user} />)
-          Object.keys(groupList).map((group) => (
-            <div className={"groupWrapper"} key={group}>
-              <div className={"groupName"}>
-                {`Registered in ${group}`}
-                <div className={"separator"} />
-              </div>
-              {groupList[group].map((user, i) => (
-                <UserCard key={i} user={user} />
-              ))}
-            </div>
-          ))
+            Object.keys(groupList).map(group => <div className={"groupWrapper"}>
+                <div className={"groupName"}>{`Registered in ${group}`}<div className={"separator"}/></div>
+                {groupList[group].map((user, i) => <UserCard key={i} user={user} />)}
+            </div>)
         )}
       </div>
     </div>
